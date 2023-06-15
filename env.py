0d27f149e6c9939cd2f968cd84dd5dcb9d19f1c7
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-06-15 21:32:55 +0700

    Update and rename .env to env.py

---
+++ env.py
@@ -0,0 +1,11 @@
+# @MSDZULQRNN
+# t.me/MSPR0JECT | t.me/envSample
+# KONTOL LU CLONER
+
+API_ID = "16452568"
+API_HASH = "f936697c5c9e5bffd433babef7a4e4c9"
+BOT_TOKEN = "5883022355:AAEk6wtmdeUxyajh8fxBFfjcQdiLtfQ-f3M"
+FSUB = "-1001967976141"
+thumb = "https://telegra.ph/file/bdf23d4e78c8337249c26.png"
+BOT_NAME = "ThumbnailRobot"
+CHANNEL_ID = "-1001938323171"
