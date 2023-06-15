05bcd097cb6fb2d36331a1c1cc5bb6e94f7d34fb
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-06-15 20:51:22 +0700

    Update main.py

--- main.py
+++ main.py
@@ -15,26 +15,26 @@ Bot = Client(
 @Bot.on_message(filters.private & (filters.video | filters.document))
 async def thumb_change(bot, m):
     global thumb
-    msg = await m.reply("`Downloading..`", parse_mode='md')
+    msg = await m.reply("`Sedang Mendownload..`", parse_mode='md')
     c_time = time.time()
     file_dl_path = await bot.download_media(message=m, progress=progress_for_pyrogram, progress_args=("Downloading file..", msg, c_time))
     await msg.delete()
-    answer = await bot.ask(m.chat.id,'Now send the thumbnail' + ' or /keep to keep the previous thumb' if thumb else '', filters=filters.photo | filters.text)
+    answer = await bot.ask(m.chat.id,'Kirimkan Foto untuk dijadikan sebagai thumbnail' if thumb else '', filters=filters.photo | filters.text)
     if answer.photo:
         try:
             os.remove(thumb)
         except:
             pass
         thumb = await bot.download_media(message=answer.photo)
-    msg = await m.reply("`Uploading..`", parse_mode='md')
+    msg = await m.reply("`Sedang Mengupload..`", parse_mode='md')
     c_time = time.time()
     if m.document:
         await bot.send_document(chat_id=m.chat.id, document=file_dl_path, thumb=thumb, caption=m.caption if m.caption else None, progress=progress_for_pyrogram, progress_args=("Uploading file..", msg, c_time))
     elif m.video:
-        await bot.send_video(chat_id=m.chat.id, video=file_dl_path, thumb=thumb, caption=m.caption if m.caption else None, progress=progress_for_pyrogram, progress_args=("Uploading file..", msg, c_time))
+        await bot.send_video(chat_id=m.chat.id, video=file_dl_path, thumb=thumb, caption=("Add Thumbnail by @ThumbnailRobot"), progress=progress_for_pyrogram, progress_args=("Upload..", msg, c_time))
     await msg.delete()
     os.remove(file_dl_path)
 
 
-
+print("Bot Berhasil Diaktifkan 🔥🔥🔥")
 Bot.run()
