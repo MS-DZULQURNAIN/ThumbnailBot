c02e5856888c0d56818afdf9588a3dff7433eedd
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-06-15 18:16:26 +0700

    Update fsub.py

--- DzThumbnailBot/fsub.py
+++ DzThumbnailBot/fsub.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
-from config import FSUB, BOT_USERNAME
+from config import FSUB, BOT_NAME
 
 @Bot.on_message(filters.incoming & filters.private, group=-1)
 async def fsub(bot, message):
@@ -14,7 +14,7 @@ async def fsub(bot, message):
       link = await bot.export_chat_invite_link(FSUB)
     else:
       link = f"https://t.me/{FSUB}"
-    tfsub = f"👋Halo {message.from_user.mention}\n\nSebelum menggunakan {BOT_USERNAME} kamu harus subscribe atau join channel dibawah ini jika sudah klik coba lagi💡"
+    tfsub = f"👋Halo {message.from_user.mention}\n\nSebelum menggunakan {BOT_NAME} kamu harus subscribe atau join channel dibawah ini jika sudah klik coba lagi💡"
     bfsub = InlineKeyboardMarkup([
                                   [InlineKeyboardButton(text="Join Channel", url=link),],
                                   [InlineKeyboardButton(text="Coba lagi", url=f"https://t.me/{bot.username}?start={message.command[1]}")]
