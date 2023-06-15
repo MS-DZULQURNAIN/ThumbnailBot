35d2f2d66fb361f024e2b91f41b9fe1ae0d7dab1
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-06-15 18:14:47 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,11 @@
-import os
+from os import getenv
+from dotenv import load_dotenv
 
+load_dotenv(".env")
 
-# BOT TOKEN
-TOKEN = os.environ.get("BOT_TOKEN", "")
+BOT_TOKEN = getenv("BOT_TOKEN")
+API_ID = int(getenv("API_ID") or 0)
+API_HASH = getenv("API_HASH")
+FSUB = int(getenv("FSUB") or 0)
+BOT_USERNAME = getenv("BOT_USERNAME
 
-#Your API ID from my.telegram.org
-APP_ID = int(os.environ.get("APP_ID", "16452568"))
-
-#Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "f936697c5c9e5bffd433babef7a4e4c9")
-
-#Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001749379103"))
