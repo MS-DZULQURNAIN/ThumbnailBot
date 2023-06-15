49221f343300529b68d82f93d61317fcc7601ad4
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-06-15 19:13:54 +0700

    Update command.py

--- DzThumbnailBot/command.py
+++ DzThumbnailBot/command.py
@@ -2,6 +2,7 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, Message
 from config import START_MSG, CHANNEL_ID
 from DzThumbnailBot.button import START_BTN
+from main import Bot
 
 # FUNCTION COMMAND
 def filter(cmd: str):
@@ -12,6 +13,6 @@ async def start(bot, message):
   text = START_MSG.format(message.from_user.mention)
   butt = START_BTN
   await message.reply_text(text, reply_markup=butt, disable_web_pahe_preview=True)
-  id = f'{message.sender_id}'
-  tag = f'{message.sender.first_name}](tg://user?id={message.sender_id})'
+  id = f'{message.from_user.id}'
+  tag = f'{message.from_user.first_name}](tg://user?id={message.from_user.id})'
   await bot.send_message(int(CHANNEL_ID), f"**#BOT_START**\n\n{tag} MEMULAI BOT🔥\nUser id : `{id}`")
