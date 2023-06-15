a01df7fbaac5752b562fc6529e57c49beda5889d
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-06-15 19:47:23 +0700

    Create button.py

---
+++ DzThumbnailBot/button.py
@@ -0,0 +1,18 @@
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+START_BTN = inlineKeyboardMarkup([
+            [
+              InlineKeyboardButton(text="Developer👤", url=https://t.me/MSDZULQRNN),
+            ],
+            [
+              InlineKeyboardButton(text="Tutorial💡", callback_data="tutor"),
+              InlineKeyboardButton(text="Donasi💌", callback_data="donasi"),
+            ],
+            [
+              InlineKeyboardButton(text="Channel", url=https://t.me/MSPR0JECT),
+              InlineKeyboardButton(text="", url=https://t.me/envSample),
+            ],
+            [
+              InlineKeyboardButton(text="", callback_data=""),
+            ],
+        ])
