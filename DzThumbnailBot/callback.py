33911f4b46953f6f93d6a7dba2232ccaccb812d2
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-06-15 20:17:23 +0700

    Update callback.py

--- DzThumbnailBot/callback.py
+++ DzThumbnailBot/callback.py
@@ -1,7 +1,8 @@
 from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
 
 from main import Bot
-from button import HOME
+from button import HOME, START_BTN
+from config import START_MSG
 
 @Bot.on_callback_query()
 async def cb_handler(client: Bot, query: CallbackQuery):
@@ -13,6 +14,10 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             disable_web_page_preview=True,
             reply_markup=bhome
         )
+    elif data == "st":
+        tst = START_MSG.format(message.from_user.mention)
+        bst = START_BTN
+        await query.message.edit_text(text=tst, disable_web_page_preview=True, reply_markup=bst)
     elif data == "close":
         await query.message.delete()
         try:
