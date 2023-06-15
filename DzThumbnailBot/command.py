22f9ee6746bd03f244d5489fb681b7363cf3c66e
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-06-15 18:35:50 +0700

    Update command.py

--- DzThumbnailBot/command.py
+++ DzThumbnailBot/command.py
@@ -1,1 +1,15 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, Message
+from config import START_MSG, CHANNEL_ID
+from DzThumbnailBot.button import START_BTN
 
+# FUNCTION COMMAND
+def filter(cmd: str):
+    return filters.private & filters.incoming & filters.command(cmd)
+
+@Bot.on(filter("start"))
+async def start(bot, message):
+  text = START_MSG.format(message.from_user.mention)
+  butt = START_BTN
+  await message.reply_text(text, reply_markup=butt, disable_web_pahe_preview=True) 
+  await bot.send_message(
