786563b405fef8f9d84d38da66925d3ec4306633
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-06-15 20:08:35 +0700

    Update callback.py

--- DzThumbnailBot/callback.py
+++ DzThumbnailBot/callback.py
@@ -1,17 +1,17 @@
 from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
 
 from main import Bot
+from button import HOME
 
 @Bot.on_callback_query()
 async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "tutor":
+        bhome = HOME
         await query.message.edit_text(
-            text=f"**Tutorial ThumbnailRobot\n\n1.Kirimkan video ke ThumbnailRobot\n2.Kirimkan Thumnail/foto yg akan dijadikan Thumnail\n3.Selesai\n\nNote:\nBot hanya bisa memasang thumbnail dengan minimal durasi 20 detik,jika masih gagal silakan ajukan keluhannya ke developer👤",
+            text=f"**Tutorial ThumbnailRobot💡\n\n1.Kirimkan video ke ThumbnailRobot\n2.Kirimkan Thumnail/foto yg akan dijadikan Thumnail\n3.Selesai\n\nNote:\nBot hanya bisa memasang thumbnail dengan minimal durasi 20 detik,jika masih gagal silakan ajukan keluhannya ke developer👤",
             disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton("Tutup💝", callback_data="close")]]
-            ),
+            reply_markup=bhome
         )
     elif data == "close":
         await query.message.delete()
