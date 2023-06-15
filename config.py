bd834b67bbd7526881991f7496b561531fab819f
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-06-15 18:21:23 +0700

    Update config.py

--- config.py
+++ config.py
@@ -7,5 +7,6 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 API_ID = int(getenv("API_ID") or 0)
 API_HASH = getenv("API_HASH")
 FSUB = int(getenv("FSUB") or 0)
-BOT_USERNAME = getenv("BOT_USERNAME
-
+BOT_NAME = getenv("BOT_NAME")
+thumb = getenv("thumb", "https://telegra.ph/file/9f93ca1114a1e01b63239.jpg")
+CHANNEL_ID = int(getenv("CHANNEL_ID") or 0)
